cc53c897358319bc46174d9f9f610612340a2b11
caio schultz <caio.mazziero.schultz@gmail.com>
2026-04-17 20:02:12 -0300

    chore: cria estrutura inicial do backend do projeto e configura express

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    return res.send('Teste express');
+});
+
+app.listen(port);
