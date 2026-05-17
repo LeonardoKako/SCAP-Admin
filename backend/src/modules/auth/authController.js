63563d65789bf0a35b5fde4c5b531e73e514bbc5
caio schultz <caio.mazziero.schultz@gmail.com>
2026-05-17 01:12:40 -0300

    fix: adiciona try/catch na função de login do controller

--- backend/src/modules/auth/authController.js
+++ backend/src/modules/auth/authController.js
@@ -1,18 +1,22 @@
 import authService from "./authService.js";
 
 export async function login(req, res, next) {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    const loginBody = {
-        email,
-        password
-    };
+        const loginBody = {
+            email,
+            password
+        };
 
-    const loginResponse = await authService.login(loginBody);
+        const loginResponse = await authService.login(loginBody);
 
-    return res.status(200).json({
-        success: true,
-        message: "Login efetuado com sucesso!",
-        ...loginResponse
-    });
+        return res.status(200).json({
+            success: true,
+            message: "Login efetuado com sucesso!",
+            ...loginResponse
+        });
+    } catch(error) {
+        return next(error);
+    }
 }
